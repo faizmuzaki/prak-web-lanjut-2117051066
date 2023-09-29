a09e5cd01661125a204e6a9112bc8a20d3c4758d
Faiz Muzaki <34159640+faizmuzaki@users.noreply.github.com>
2023-09-29 09:04:55 +0700

    feat : read data user

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Profil</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-  <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
-</head>
+<?= $this->extend('layouts/app') ?>
+<?= $this->section('content') ?>
 
 <body>
   <!-- <p>Nama: Faiz Muzaki</p>
@@ -31,6 +23,4 @@
       </td>
     </tr>
   </table>
-</body>
-
-</html>
+  <?= $this->endSection('content') ?>
